8efe6994086b4d004760e4bb51bb8e643f51bbd0
Serhii Kostiushkin <s.kostyushkin@gmail.com>
2017-12-05 11:01:52 +0200

    Add rules for skipping testcases without data

--- tests/livr_rules_SUITE.erl
+++ tests/livr_rules_SUITE.erl
@@ -88,8 +88,12 @@ end_per_group(_Name, Config) ->
 
 init_per_testcase(Name, Config) ->
     Type = ?config(init_type, Config),
-    Conditions = read_conditions(Name, Type),
-    [{conditions, Conditions}|Config].
+    case read_conditions(Name, Type) of
+        {ok, Conditions} ->
+            [{conditions, Conditions}|Config];
+        skip ->
+            {skipped, no_data}
+    end.
 
 end_per_testcase(_Name, Config) ->
     lists:keydelete(conditions, 1, Config).
@@ -183,121 +187,127 @@ default(Config) ->
 
 %% internal
 read_conditions(TestCase, Option) ->
-    Path = case_to_path(TestCase, Option),
-    OutputPath = case Option of
-        positive -> "output.json";
-        negative -> "errors.json"
-    end,
-    {ok, Rules}     = file:read_file(Path ++ "rules.json"),
-    {ok, Input}     = file:read_file(Path ++ "input.json"),
-    {ok, Output}    = file:read_file(Path ++ OutputPath),
-    {decode(Rules), decode(Input), decode(Output)}.
+    case case_to_path(TestCase, Option) of
+        {ok, Path} ->
+            OutputPath = case Option of
+                positive -> "output.json";
+                negative -> "errors.json"
+            end,
+            {ok, Rules}     = file:read_file(Path ++ "rules.json"),
+            {ok, Input}     = file:read_file(Path ++ "input.json"),
+            {ok, Output}    = file:read_file(Path ++ OutputPath),
+            {ok, {decode(Rules), decode(Input), decode(Output)}};
+        skip ->
+            skip
+    end.
 
 case_to_path(required, positive) ->
-    ?LIVR_TEST_PATH ++ "/positive/01-required/";
+    {ok, ?LIVR_TEST_PATH ++ "/positive/01-required/"};
 case_to_path(required, negative) ->
-    ?LIVR_TEST_PATH ++ "/negative/01-required/";
+    {ok, ?LIVR_TEST_PATH ++ "/negative/01-required/"};
 case_to_path(not_empty, positive) ->
-    ?LIVR_TEST_PATH ++ "/positive/02-not_empty/";
+    {ok, ?LIVR_TEST_PATH ++ "/positive/02-not_empty/"};
 case_to_path(not_empty, negative) ->
-    ?LIVR_TEST_PATH ++ "/negative/02-not_empty/";
+    {ok, ?LIVR_TEST_PATH ++ "/negative/02-not_empty/"};
 case_to_path(not_empty_list, positive) ->
-    ?LIVR_TEST_PATH ++ "/positive/22-not_empty_list/";
+    {ok, ?LIVR_TEST_PATH ++ "/positive/22-not_empty_list/"};
 case_to_path(not_empty_list, negative) ->
-    ?LIVR_TEST_PATH ++ "/negative/22-not_empty_list/";
+    {ok, ?LIVR_TEST_PATH ++ "/negative/22-not_empty_list/"};
 case_to_path(any_object, positive) ->
-    ?LIVR_TEST_PATH ++ "/positive/27-any_object/";
+    {ok, ?LIVR_TEST_PATH ++ "/positive/27-any_object/"};
 case_to_path(any_object, negative) ->
-    ?LIVR_TEST_PATH ++ "/negative/27-any_object/";
+    {ok, ?LIVR_TEST_PATH ++ "/negative/27-any_object/"};
 case_to_path(string, positive) ->
-    ?LIVR_TEST_PATH ++ "/positive/26-string/";
+    {ok, ?LIVR_TEST_PATH ++ "/positive/26-string/"};
 case_to_path(string, negative) ->
-    ?LIVR_TEST_PATH ++ "/negative/26-string/";
+    {ok, ?LIVR_TEST_PATH ++ "/negative/26-string/"};
 case_to_path(eq, positive) ->
-    ?LIVR_TEST_PATH ++ "/positive/25-eq/";
+    {ok, ?LIVR_TEST_PATH ++ "/positive/25-eq/"};
 case_to_path(eq, negative) ->
-    ?LIVR_TEST_PATH ++ "/negative/25-eq/";
+    {ok, ?LIVR_TEST_PATH ++ "/negative/25-eq/"};
 case_to_path(one_of, positive) ->
-    ?LIVR_TEST_PATH ++ "/positive/03-one_of/";
+    {ok, ?LIVR_TEST_PATH ++ "/positive/03-one_of/"};
 case_to_path(one_of, negative) ->
-    ?LIVR_TEST_PATH ++ "/negative/03-one_of/";
+    {ok, ?LIVR_TEST_PATH ++ "/negative/03-one_of/"};
 case_to_path(max_length, positive) ->
-    ?LIVR_TEST_PATH ++ "/positive/05-max_length/";
+    {ok, ?LIVR_TEST_PATH ++ "/positive/05-max_length/"};
 case_to_path(max_length, negative) ->
-    ?LIVR_TEST_PATH ++ "/negative/05-max_length/";
+    {ok, ?LIVR_TEST_PATH ++ "/negative/05-max_length/"};
 case_to_path(min_length, positive) ->
-    ?LIVR_TEST_PATH ++ "/positive/04-min_length/";
+    {ok, ?LIVR_TEST_PATH ++ "/positive/04-min_length/"};
 case_to_path(min_length, negative) ->
-    ?LIVR_TEST_PATH ++ "/negative/04-min_length/";
+    {ok, ?LIVR_TEST_PATH ++ "/negative/04-min_length/"};
 case_to_path(length_between, positive) ->
-    ?LIVR_TEST_PATH ++ "/positive/07-length_between/";
+    {ok, ?LIVR_TEST_PATH ++ "/positive/07-length_between/"};
 case_to_path(length_between, negative) ->
-    ?LIVR_TEST_PATH ++ "/negative/07-length_between/";
+    {ok, ?LIVR_TEST_PATH ++ "/negative/07-length_between/"};
 case_to_path(length_equal, positive) ->
-    ?LIVR_TEST_PATH ++ "/positive/06-length_equal/";
+    {ok, ?LIVR_TEST_PATH ++ "/positive/06-length_equal/"};
 case_to_path(length_equal, negative) ->
-    ?LIVR_TEST_PATH ++ "/negative/06-length_equal/";
+    {ok, ?LIVR_TEST_PATH ++ "/negative/06-length_equal/"};
 case_to_path(like, positive) ->
-    ?LIVR_TEST_PATH ++ "/positive/08-like/";
+    {ok, ?LIVR_TEST_PATH ++ "/positive/08-like/"};
 case_to_path(like, negative) ->
-    ?LIVR_TEST_PATH ++ "/negative/08-like/";
+    {ok, ?LIVR_TEST_PATH ++ "/negative/08-like/"};
 case_to_path(integer, positive) ->
-    ?LIVR_TEST_PATH ++ "/positive/09-integer/";
+    {ok, ?LIVR_TEST_PATH ++ "/positive/09-integer/"};
 case_to_path(integer, negative) ->
-    ?LIVR_TEST_PATH ++ "/negative/09-integer/";
+    {ok, ?LIVR_TEST_PATH ++ "/negative/09-integer/"};
 case_to_path(positive_integer, positive) ->
-    ?LIVR_TEST_PATH ++ "/positive/10-positive_integer/";
+    {ok, ?LIVR_TEST_PATH ++ "/positive/10-positive_integer/"};
 case_to_path(positive_integer, negative) ->
-    ?LIVR_TEST_PATH ++ "/negative/10-positive_integer/";
+    {ok, ?LIVR_TEST_PATH ++ "/negative/10-positive_integer/"};
 case_to_path(decimal, positive) ->
-    ?LIVR_TEST_PATH ++ "/positive/11-decimal/";
+    {ok, ?LIVR_TEST_PATH ++ "/positive/11-decimal/"};
 case_to_path(decimal, negative) ->
-    ?LIVR_TEST_PATH ++ "/negative/11-decimal/";
+    {ok, ?LIVR_TEST_PATH ++ "/negative/11-decimal/"};
 case_to_path(positive_decimal, positive) ->
-    ?LIVR_TEST_PATH ++ "/positive/12-positive_decimal/";
+    {ok, ?LIVR_TEST_PATH ++ "/positive/12-positive_decimal/"};
 case_to_path(positive_decimal, negative) ->
-    ?LIVR_TEST_PATH ++ "/negative/12-positive_decimal/";
+    {ok, ?LIVR_TEST_PATH ++ "/negative/12-positive_decimal/"};
 case_to_path(max_number, positive) ->
-    ?LIVR_TEST_PATH ++ "/positive/13-max_number/";
+    {ok, ?LIVR_TEST_PATH ++ "/positive/13-max_number/"};
 case_to_path(max_number, negative) ->
-    ?LIVR_TEST_PATH ++ "/negative/13-max_number/";
+    {ok, ?LIVR_TEST_PATH ++ "/negative/13-max_number/"};
 case_to_path(min_number, positive) ->
-    ?LIVR_TEST_PATH ++ "/positive/14-min_number/";
+    {ok, ?LIVR_TEST_PATH ++ "/positive/14-min_number/"};
 case_to_path(min_number, negative) ->
-    ?LIVR_TEST_PATH ++ "/negative/14-min_number/";
+    {ok, ?LIVR_TEST_PATH ++ "/negative/14-min_number/"};
 case_to_path(number_between, positive) ->
-    ?LIVR_TEST_PATH ++ "/positive/15-number_between/";
+    {ok, ?LIVR_TEST_PATH ++ "/positive/15-number_between/"};
 case_to_path(number_between, negative) ->
-    ?LIVR_TEST_PATH ++ "/negative/15-number_beetween/";
+    {ok, ?LIVR_TEST_PATH ++ "/negative/15-number_beetween/"};
 case_to_path(nested_object, positive) ->
-    ?LIVR_TEST_PATH ++ "/positive/18-nested_object/";
+    {ok, ?LIVR_TEST_PATH ++ "/positive/18-nested_object/"};
 case_to_path(nested_object, negative) ->
-    ?LIVR_TEST_PATH ++ "/negative/18-nested_object/";
+    {ok, ?LIVR_TEST_PATH ++ "/negative/18-nested_object/"};
 case_to_path(list_of, positive) ->
-    ?LIVR_TEST_PATH ++ "/positive/19-list_of/";
+    {ok, ?LIVR_TEST_PATH ++ "/positive/19-list_of/"};
 case_to_path(list_of, negative) ->
-    ?LIVR_TEST_PATH ++ "/negative/19-list_of/";
+    {ok, ?LIVR_TEST_PATH ++ "/negative/19-list_of/"};
 case_to_path(list_of_objects, positive) ->
-    ?LIVR_TEST_PATH ++ "/positive/20-list_of_objects/";
+    {ok, ?LIVR_TEST_PATH ++ "/positive/20-list_of_objects/"};
 case_to_path(list_of_objects, negative) ->
-    ?LIVR_TEST_PATH ++ "/negative/20-list_of_objects/";
+    {ok, ?LIVR_TEST_PATH ++ "/negative/20-list_of_objects/"};
 case_to_path(list_of_different_objects, positive) ->
-    ?LIVR_TEST_PATH ++ "/positive/21-list_of_different_objects/";
+    {ok, ?LIVR_TEST_PATH ++ "/positive/21-list_of_different_objects/"};
 case_to_path(list_of_different_objects, negative) ->
-    ?LIVR_TEST_PATH ++ "/negative/21-list_of_different_objects/";
+    {ok, ?LIVR_TEST_PATH ++ "/negative/21-list_of_different_objects/"};
 
 case_to_path(trim, positive) ->
-    ?LIVR_TEST_PATH ++ "/positive/30-trim/";
+    {ok, ?LIVR_TEST_PATH ++ "/positive/30-trim/"};
 case_to_path(to_lc, positive) ->
-    ?LIVR_TEST_PATH ++ "/positive/31-to_lc/";
+    {ok, ?LIVR_TEST_PATH ++ "/positive/31-to_lc/"};
 case_to_path(to_uc, positive) ->
-    ?LIVR_TEST_PATH ++ "/positive/32-to_uc/";
+    {ok, ?LIVR_TEST_PATH ++ "/positive/32-to_uc/"};
 case_to_path(remove, positive) ->
-    ?LIVR_TEST_PATH ++ "/positive/33-remove/";
+    {ok, ?LIVR_TEST_PATH ++ "/positive/33-remove/"};
 case_to_path(leave_only, positive) ->
-    ?LIVR_TEST_PATH ++ "/positive/34-leave_only/";
+    {ok, ?LIVR_TEST_PATH ++ "/positive/34-leave_only/"};
 case_to_path(default, positive) ->
-    ?LIVR_TEST_PATH ++ "/positive/35-default/".
+    {ok, ?LIVR_TEST_PATH ++ "/positive/35-default/"};
+case_to_path(_, _) ->
+    skip.
 
 expected_output(positive, Output) ->
     {ok, Output};
