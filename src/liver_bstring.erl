7fb9cd8911df5c1676f5952bbfea49e13155c47f
Serhii Kostiushkin <s.kostyushkin@gmail.com>
2017-12-08 00:45:48 +0200

    Add Unicode support in to_lower and to_upper rules

--- src/liver_bstring.erl
+++ src/liver_bstring.erl
@@ -10,8 +10,8 @@
 -export([trim_right/1]).
 -export([to_lower/1]).
 -export([to_upper/1]).
--export([char_to_lower/1]).
--export([char_to_upper/1]).
+
+-include("unicode.hrl").
 
 -define(IS_BLANK(Blank),
     Blank == $\s;
@@ -32,7 +32,7 @@ when
     PartAfterPattern    :: binary_string().
 split(BinString, Pattern) ->
     case binary:match(BinString, Pattern) of
-        {A,B} ->
+        {A, B} ->
             <<Before:A/binary, _:B/binary, After/binary>> = BinString,
             {Before, After};
         nomatch ->
@@ -128,77 +128,16 @@ trim_right(BinString) ->
             BinString
     end.
 
-
-
 -spec to_lower(BinString1) -> BinString2
 when
     BinString1 :: binary_string(),
     BinString2 :: binary_string().
 to_lower(BinString) ->
-    << <<(char_to_lower(C))>> || <<C>> <= BinString>>.
+    << <<(maps:get(C, ?TO_LOWER, C))/utf8>> || <<C/utf8>> <= BinString>>.
 
 -spec to_upper(BinString1) -> BinString2
 when
     BinString1 :: binary_string(),
     BinString2 :: binary_string().
 to_upper(BinString) ->
-    << <<(char_to_upper(C))>> || <<C>> <= BinString>>.
-
-%% TODO Add unicode support
--spec char_to_lower(char()) -> char().
-char_to_lower($A) -> $a;
-char_to_lower($B) -> $b;
-char_to_lower($C) -> $c;
-char_to_lower($D) -> $d;
-char_to_lower($E) -> $e;
-char_to_lower($F) -> $f;
-char_to_lower($G) -> $g;
-char_to_lower($H) -> $h;
-char_to_lower($I) -> $i;
-char_to_lower($J) -> $j;
-char_to_lower($K) -> $k;
-char_to_lower($L) -> $l;
-char_to_lower($M) -> $m;
-char_to_lower($N) -> $n;
-char_to_lower($O) -> $o;
-char_to_lower($P) -> $p;
-char_to_lower($Q) -> $q;
-char_to_lower($R) -> $r;
-char_to_lower($S) -> $s;
-char_to_lower($T) -> $t;
-char_to_lower($U) -> $u;
-char_to_lower($V) -> $v;
-char_to_lower($W) -> $w;
-char_to_lower($X) -> $x;
-char_to_lower($Y) -> $y;
-char_to_lower($Z) -> $z;
-char_to_lower(C)  -> C.
-
--spec char_to_upper(char()) -> char().
-char_to_upper($a) -> $A;
-char_to_upper($b) -> $B;
-char_to_upper($c) -> $C;
-char_to_upper($d) -> $D;
-char_to_upper($e) -> $E;
-char_to_upper($f) -> $F;
-char_to_upper($g) -> $G;
-char_to_upper($h) -> $H;
-char_to_upper($i) -> $I;
-char_to_upper($j) -> $J;
-char_to_upper($k) -> $K;
-char_to_upper($l) -> $L;
-char_to_upper($m) -> $M;
-char_to_upper($n) -> $N;
-char_to_upper($o) -> $O;
-char_to_upper($p) -> $P;
-char_to_upper($q) -> $Q;
-char_to_upper($r) -> $R;
-char_to_upper($s) -> $S;
-char_to_upper($t) -> $T;
-char_to_upper($u) -> $U;
-char_to_upper($v) -> $V;
-char_to_upper($w) -> $W;
-char_to_upper($x) -> $X;
-char_to_upper($y) -> $Y;
-char_to_upper($z) -> $Z;
-char_to_upper(C)  -> C.
+    << <<(maps:get(C, ?TO_UPPER, C))/utf8>> || <<C/utf8>> <= BinString>>.
